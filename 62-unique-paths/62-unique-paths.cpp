fc1451c549bb0f0cec30641ab7857718cd8e495c
Aanshi Singh <90664389+Aanshi-Singh@users.noreply.github.com>
2022-06-13 01:30:06 +0530

    Time: 3 ms (42.66%), Space: 5.9 MB (90.45%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    int paths(int m, int n, int i, int j, vector<vector<int>> &dp){
-        
-        if(i>=n || j>=m){
-            return 0;
-        }
-
-        if(i == (n-1) && j==(m-1)){
-            return 1;
-        }
-        
-        if(dp[i][j]!= -1)
-        {
-            return dp[i][j];}
-        else{
-        
-            dp[i][j] = paths(m , n, (i+1), j,dp)+paths(m, n, i, j+1,dp);
-            return dp[i][j];
-        }
-    }
     int uniquePaths(int m, int n) {
-            vector <vector <int>> dp(n, vector<int> (m, -1));
-
-        return paths(m, n, 0, 0, dp);
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        for(int i = 1; i<= r; i++){
+        res = res* (N-r+i)/i;
+        }return res;
     }
 };
